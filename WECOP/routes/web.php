aafc41aaccab59b8f2fc5c3218c2f29267932e0d
Federico Perez <federicoperez61@gmail.com>
2021-03-14 12:03:16 -0500

    Review routes added

--- WECOP/routes/web.php
+++ WECOP/routes/web.php
@@ -15,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
 Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
+Route::get('/review/show/{id}', 'App\Http\Controllers\ReviewController@show')->name("review.show");
+Route::get('/review/create', 'App\Http\Controllers\ReviewController@create')->name("review.create");
+Route::get('/review/list', 'App\Http\Controllers\ReviewController@list')->name("review.list");
+Route::get('/review/delete/{id}', 'App\Http\Controllers\ReviewController@delete')->name("review.delete");
+
+Route::post('/review/save', 'App\Http\Controllers\ReviewController@save')->name("review.save");
 
 Auth::routes();
