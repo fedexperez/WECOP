99f3f1289334f15bd223ccf89ec14637dadd2e71
Federico Perez <federicoperez61@gmail.com>
2021-03-19 07:32:39 -0500

    If statement deleted

--- WECOP/resources/views/review/list.blade.php
+++ WECOP/resources/views/review/list.blade.php
@@ -5,15 +5,9 @@
     <div class="col-md-12">
         <ul id="errors">
             @foreach($data["reviews"] as $review)
-            @if($loop->index < 2)
-            <li>
-                @lang('messages.Review') <a href="{{ route('review.show', $review->getId()) }}"> <b> {{ $review->getId() }} </b> </a>
-            </li>
-            @else 
             <li>
                 @lang('messages.Review') <a href="{{ route('review.show', $review->getId()) }}"> {{ $review->getId() }} </a>
             </li>
-            @endif
             @endforeach
         </ul>
     </div>
