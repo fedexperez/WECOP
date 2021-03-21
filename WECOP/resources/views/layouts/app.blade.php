c6ff667d540ae017529c86f514a9a10cb0c424c0
clopezr9 <46933189+clopezr9@users.noreply.github.com>
2021-03-21 14:26:45 -0500

    Update app.blade.php
    deleting duplicated code

--- WECOP/resources/views/layouts/app.blade.php
+++ WECOP/resources/views/layouts/app.blade.php
@@ -36,25 +36,6 @@
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li>
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>@yield('title','Home Page')</title>
-    <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="{{ asset('/img/favicon.ico') }}" />
-    <!-- Font Awesome icons (free version)-->
-    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
-    <!-- Google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet"
-        type="text/css" />
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="{{ asset('/css/styles.css') }}" rel="stylesheet" />
-    <link href="{{ asset('/css/custom-styles.css') }}" rel="stylesheet" />
-</head>
-
 <body id="page-top">
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
