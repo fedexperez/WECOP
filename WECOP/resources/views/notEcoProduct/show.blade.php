4437b8e799c52358f36aaa16d46924d824916c10
clopezr <clopezr9@eafit.edu.co>
2021-03-21 01:15:21 -0500

    changes in views

--- WECOP/resources/views/notEcoProduct/show.blade.php
+++ WECOP/resources/views/notEcoProduct/show.blade.php
@@ -10,10 +10,9 @@
             <div class="card">
                 <div class="card-header">{{ $data["notEcoProduct"]["name"] }}</div>
                 <div class="card-body">
-                    <strong>Product name:</strong> {{ $data["notEcoProduct"]["name"] }}<br />
-                    <strong>Product price:</strong> {{ $data["notEcoProduct"]["price"] }}<br />
-                    <strong>Product emision:</strong> {{ $data["notEcoProduct"]["emision"] }}<br />
-                    <strong>Product life:</strong> {{ $data["notEcoProduct"]["productLife"] }}<br />
+                    <strong>Product name:</strong> {{ $data["notEcoProduct"]->getName() }}<br />
+                    <strong>Product emision:</strong> {{ $data["notEcoProduct"]->getEmision() }}<br />
+                    <strong>Product life:</strong> {{ $data["notEcoProduct"]->getProductLife() }}<br />
                     <ul>
                     </ul>
                     <a href="{{ route('notEcoProduct.delete', ['id' => $data['notEcoProduct']['id']]) }}"> Delete </a>
