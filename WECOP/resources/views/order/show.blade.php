46151ea2e506a654316b99fb3ac27feb06f20295
Federico Perez <federicoperez61@gmail.com>
2021-03-21 11:26:25 -0500

    Order views created

---
+++ WECOP/resources/views/order/show.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.master')
+
+@section("title", $data["order"]->getTitle())
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"> @lang('messages.Review') {{ $data["order"]->getId() }}</div>
+                <div class="card-body">
+                    <b>@lang('messages.Status'): </b> {{ $data["order"]->getStatus() }}<br />
+                    <b>@lang('messages.PaymentType'): </b>{{ $data["order"]->getPaymentType() }}<br />
+                    <b>@lang('messages.Address'): </b>{{ $data["order"]->getAddress() }}<br />
+                    <b>@lang('messages.Date'): </b>{{ $data["order"]->getDate() }}<br />
+                    <b>@lang('messages.Total'): </b>{{ $data["order"]->getTotal() }}<br /><br />
+                </div>
+                <a href="{{ route('order.delete', $data['order']->getId()) }}"> @lang('messages.Delete') </a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
