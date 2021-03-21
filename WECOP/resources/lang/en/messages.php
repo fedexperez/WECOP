ce59bf32b8e074d44c762a8dd26df850a1ee2864
Federico Perez <federicoperez61@gmail.com>
2021-03-21 11:41:51 -0500

    Messages file

---
+++ WECOP/resources/lang/en/messages.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'HomePage' => 'Home Page',
+    'Delete' => 'Delete',
+
+    //For reviews
+    'CreateReview' => 'Create Review',
+    'RatingInput' => 'Rating 1 to 5',
+    'TitleInput' => 'Enter Title',
+    'MessageInput' => 'Enter Message',
+    'Rating' => 'Rating',
+    'Review' => 'Review',
+    'Title' => 'Title',
+    'Message' => 'Message',
+    'SuccesfullReview' => 'Review successfully created',
+    'Return' => 'Return',
+    'Great' => 'Great',
+    'Send' => 'Send',
+    'Order' => 'Order',
+    'Status' => 'Status',
+    'PaymentType' => 'Payment Type',
+    'Address' => 'Address',
+    'Total' => 'Total',
+    
+    
+
+];
