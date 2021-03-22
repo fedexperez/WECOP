d8ed4f68e3d29255e3876937bdc0805249da3ac0
clopezr <clopezr9@eafit.edu.co>
2021-03-22 14:31:33 -0500

    idk

--- WECOP/app/Models/User.php
+++ WECOP/app/Models/User.php
@@ -11,6 +11,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Http\Request;
 
 class User extends Authenticatable
 {
