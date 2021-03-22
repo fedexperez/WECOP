f2d195a1411d3d5dafb8b76d0cbbe4a84e5fef1e
clopezr <clopezr9@eafit.edu.co>
2021-03-22 18:22:04 -0500

    correctiosn to admin views steps

--- WECOP/resources/views/admin/notEcoProduct/create.blade.php
+++ WECOP/resources/views/admin/notEcoProduct/create.blade.php
@@ -6,7 +6,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-        @include('util.message')
+            @include('util.message')
             <div class="card">
                 <div class="card-header">Create notEcoProduct</div>
                 <div class="card-body">
@@ -21,9 +21,12 @@
                     <form method="POST" action="{{ route('admin.notEcoProduct.save') }}">
                         @csrf
                         <input type="text" placeholder="Enter name" name="name" value="{{ old('name') }}" />
-                        <input type="number" placeholder="Enter price" name="price" value="{{ old('price') }}" />
-                        <input type="number" placeholder="Enter emision" name="emision" value="{{ old('emision') }}" />
-                        <input type="number" placeholder="Enter product life" name="product_life" value="{{ old('product_life') }}" />
+                        <input type="number" step="0.01" placeholder="Enter price" name="price"
+                            value="{{ old('price') }}" />
+                        <input type="number" step="0.01" placeholder="Enter emision" name="emision"
+                            value="{{ old('emision') }}" />
+                        <input type="number" placeholder="Enter product life" name="product_life"
+                            value="{{ old('product_life') }}" />
                         <input type="submit" value="Send" />
                     </form>
                 </div>
