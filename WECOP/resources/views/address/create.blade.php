7f0a838eaa6aefbd80594a300ec72fa4022dc91d
Shiroke-013 <Shiroke4321@gmail.com>
2021-03-29 18:52:40 -0500

    Address complete

--- WECOP/resources/views/address/create.blade.php
+++ WECOP/resources/views/address/create.blade.php
@@ -9,24 +9,72 @@
             @include('util.message')
             <div class="card">
                 <div class="card-header">Create an Address</div>
-                <div class="card-body">
-                    @if($errors->any())
-                    <ul id="errors">
-                        @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                    @endif
+                    <div class="card-body">
+                        <form method="POST" action="{{  route('address.save') }}">
+                            @csrf
 
-                    <form method="POST" action="{{ route('address.save') }}">
-                        @csrf
-                        <input type="text" placeholder="Enter country" name="country" value="{{ old('country') }}" /> <br>
-                        <input type="text" placeholder="Enter city" name="city" value="{{ old('city') }}" /> <br>
-                        <input type="text" placeholder="Enter address" name="address" value="{{ old('address') }}" /> <br>
-                        <input type="text" placeholder="Enter postal code" name="postal_code" value="{{ old('postal_code') }}" />
-                        <input type="submit" value="@lang('messages.Send')" />
+                            <div class="form-group row">
+                                <label for="country" class="col-md-4 col-form-label text-md-right">{{ __('Country') }}</label>
+                                <div class="col-md-6">
+                                    <input id="country" type="text" class="form-control @error('country') is-invalid @enderror" name="country" value="{{ old('country') }}" required autocomplete="country" autofocus>
+                                    @error('country')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="city" class="col-md-4 col-form-label text-md-right">{{ __('City') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="city" type="text" class="form-control @error('city') is-invalid @enderror" name="city" value="{{ old('city') }}" required autocomplete="city" autofocus>
+
+                                    @error('city')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Address') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="address" type="address" class="form-control @error('address') is-invalid @enderror" name="address" required autocomplete="new-address">
+
+                                    @error('address')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="postal_code" class="col-md-4 col-form-label text-md-right">{{ __('Postal Code') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="postal_code" type="postal_code" class="form-control @error('postal_code') is-invalid @enderror" name="postal_code" value="{{ old('postal_code') }}" required autocomplete="postal_code">
+
+                                    @error('postal_code')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" value="@lang('messages.Send')" class="btn btn-primary">
+                                    {{ __('Create') }}
+                                </button>
+                            </div>
+                        </div>  
                     </form>
-
                 </div>
             </div>
         </div>
