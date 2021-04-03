219992b213e4f66e8dea2f373c85096fe250e043
Shiroke-013 <Shiroke4321@gmail.com>
2021-04-03 18:54:12 -0500

    Href Image in results blade

--- WECOP/resources/views/searchBar/results.blade.php
+++ WECOP/resources/views/searchBar/results.blade.php
@@ -7,21 +7,21 @@
             @foreach ($data['ecoProducts'] as $ecoProduct)
                 <h1 class="text-center text-uppercase text-secondary">{{ $ecoProduct->getName() }}</h1>
                 <div class="row">
-                        <div class="col-md-6 col-lg-4">
-                            <a class="row" href="{{ route('ecoProduct.show', $ecoProduct->getId()) }}">
-                                <img class="img-fluid" src="{{ url('img/ecoProducts/'.$ecoProduct->getPhoto()) }}" alt="product"/>
-                            </a>
-                        </div>
-                        <div class="col-md-6 col-lg-8 mb-5">
-                            <p class="lead">{{ $ecoProduct->getDescription() }}</p> 
-                            <p class="lead">$ {{ $ecoProduct->getPrice() }}</p>
-                            @if( $ecoProduct->getStock() > 0)
-                                <p class="lead" style="color:green">In stock!</p><br>
-                            @else
-                                <p class="lead" style="color:red">Out of stock :(</p><br>
-                            @endif
-                                <p class="lead">{{ $ecoProduct->getFacts() }}</p><br>
-                        </div>
+                    <div class="col-md-6 col-lg-4">
+                        <a href="{{ route('ecoProduct.show', $ecoProduct->getId()) }}">
+                            <img class="img-fluid" src="{{ url('img/ecoProducts/'.$ecoProduct->getPhoto()) }}" alt="product"/>
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-lg-8 mb-5">
+                        <p class="lead">{{ $ecoProduct->getDescription() }}</p> 
+                        <p class="lead">$ {{ $ecoProduct->getPrice() }}</p>
+                        @if( $ecoProduct->getStock() > 0)
+                            <p class="lead" style="color:green">In stock!</p><br>
+                        @else
+                            <p class="lead" style="color:red">Out of stock :(</p><br>
+                        @endif
+                            <p class="lead">{{ $ecoProduct->getFacts() }}</p><br>
+                    </div>
                 </div>
             @endforeach
         @else 
