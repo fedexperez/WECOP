a636c09a474ca1f7c90708458bb318bfe3984bad
Federico Perez <federicoperez61@gmail.com>
2021-04-17 10:29:54 -0500

    Laravel lang changes

--- WECOP/resources/views/review/success.blade.php
+++ WECOP/resources/views/review/success.blade.php
@@ -5,14 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">@lang('messages.Great')</div>
+                <div class="card-header">@lang('review.Great')</div>
 
                 <div class="card-body">
-                    @lang('messages.SuccesfullReview')
+                    @lang('review.SuccesfullReview')
                 </div>
 
                 <a class="button" href="{{ route('review.create') }}">
-                    @lang('messages.Return')
+                    @lang('review.Return')
                 </a>
             </div>
         </div>
