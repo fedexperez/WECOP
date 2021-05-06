3eb5c1f376d940064e7fcf2765eade78f0e9aef0
Federico Perez <federicoperez61@gmail.com>
2021-05-06 13:36:14 -0500

    Changed visual style so all have the same style

--- WECOP/resources/views/address/show.blade.php
+++ WECOP/resources/views/address/show.blade.php
@@ -4,26 +4,23 @@
 <section class="page-section">
     @include('util.breadcrumbs')
     <div class="container">
-        <h1 class="text-center text-uppercase text-secondary">{{ $data['address']->getAddress() }}</h1>
-        <!-- Icon Divider-->
-        <div class="divider-custom">
-            <div class="divider-custom-line"></div>
-            <div class="divider-custom-icon"><i class="fas fa-globe-americas"></i></div>
-            <div class="divider-custom-line"></div>
-        </div>
-        <div class="text-center">
-            <div class="col-md-6 col-lg-8 mb-5">
-                <p class="lead">@lang('messages.country'): {{ $data['address']->getCountry() }}</p>
-                <p class="lead">@lang('messages.city'): {{ $data['address']->getCity() }}</p>
-                <p class="lead">@lang('messages.postal_code'): {{ $data['address']->getPostalCode() }}</p><br>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <br>
+                <h2 class="text-center text-uppercase text-secondary">{{ $data['address']->getAddress() }}</h2>
+                <div class="card-body">
+                    <b>@lang('messages.country'): </b> {{ $data['address']->getCountry() }}<br>
+                    <b>@lang('messages.city'): </b>{{ $data['address']->getCity() }}<br>
+                    <b>@lang('messages.postal_code'): </b>{{ $data['address']->getPostalCode() }}<br><br>
+                </div>
+                <div class="text-center">
+                    <form action="{{ route('address.delete', $data['address']->getId()) }}">
+                        <button type="submit" class="btn btn-primary">
+                            @lang('messages.delete')
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div>
-        <div class="text-center">
-            <form action="{{ route('address.delete', $data['address']->getId()) }}">
-                <button type="submit" class="btn btn-primary">
-                    @lang('messages.delete')
-                </button>
-            </form>
         </div>
     </div>
 </section>
